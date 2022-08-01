0ba0925d0396c92b37b9449e42ddbefc7fcecfb6
Gleb Pashchenko <pashenkogleb@gmail.com>
2022-08-01 17:13:50 +0400

    improved is_same

--- gutil/metaprogramming.hpp
+++ gutil/metaprogramming.hpp
@@ -5,18 +5,17 @@
 
 namespace gutil{
 
+template <typename T, typename... Args>
+struct all_same;
+
+template <typename T>
+struct all_same<T>{
+    static const bool value = true; // if just one type lets that it is true 
+};  
+
 template <typename T, typename Arg, typename... Args> // check that all types after T are equal to T
-struct all_same{
+struct all_same<T, Arg, Args...>{
     static const bool value = std::is_same<T, Arg>::value && all_same<T, Args...>::value ;
 };
-
-
-
-template <typename T, typename Arg>
-struct all_same<T, Arg>{
-    static const bool value = std::is_same<T, Arg>::value;
-};
-
-
 
 }
